39bddae94a6cc68f644a67199380465770ef3162
sreedharrao@outlook.com <sreedharrao@outlook.com>
2020-06-19 16:31:02 +0100

    setting up packer

---
+++ mgmt_host/setup_packer.sh
@@ -0,0 +1,5 @@
+# create packer user
+#create ffile structure for packer
+#assign the user to proper group and giver right permissions
+#download packer and extract that to the right location
+#add that path
